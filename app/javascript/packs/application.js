082dfa32707a7d86f9c10af019565c15c3c2e2de
student-mckim <mckim01@email.franklin.edu>
2021-10-08 00:44:34 -0400

    adjustments nav

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,15 @@ import Rails, { $ } from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '@doabit/semantic-ui-sass'
-import 'jquery'
-
-$(document).on('turbolinks:load', function() {
-    $('.ui.dropdown').dropdown();
-  })
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "@doabit/semantic-ui-sass"
+import "jquery"
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
